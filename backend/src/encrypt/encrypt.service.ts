ad51b854ad88637658a1f4446961e0e925fbe024
Artem Dmysh <dmshh12@gmail.com>
2026-04-01 19:51:00 +0200

    feat: executed encapsulation and add class properties

--- backend/src/encrypt/encrypt.service.ts
+++ backend/src/encrypt/encrypt.service.ts
@@ -6,24 +6,28 @@ import { randomInt } from 'node:crypto'
 
 @Injectable()
 export class EncryptService {
-    
+        private readonly password = process.env.ENCRYPTION_PASSWORD as string
+        private readonly salt = process.env.ENCRYPTION_SALT as string
 
     
     async encryptCardNumber(cardNumber: string) {
         const iv = randomBytes(16);
-        const password = 'Password used to generate key';
+        const encrypt = await this.encryption(iv, cardNumber)
 
-        const key = (await promisify(scrypt)(password, 'salt', 32)) as Buffer;
-        const cipher = createCipheriv('aes-256-ctr', key, iv);
-
-        const textToEncrypt = cardNumber;
-        const encryptedText = Buffer.concat([
-        cipher.update(textToEncrypt),
-        cipher.final(),
-        ])
-
-        return `${iv.toString('hex')}:${encryptedText.toString('base64')}`
+        return `${iv.toString('hex')}:${encrypt.toString('base64')}`
     }
+
+    private async encryption(iv: Buffer, cardNumber: string) {
+            const key = (await promisify(scrypt)(this.password, this.salt, 32)) as Buffer;
+            const cipher = createCipheriv('aes-256-ctr', key, iv);
+            const textToEncrypt = cardNumber;
+            const encryptedText = Buffer.concat([
+            cipher.update(textToEncrypt),
+            cipher.final(),
+            ])
+            
+            return encryptedText
+        }
 
     async generateCardNumber() {
         let myCardNumber = '21'
